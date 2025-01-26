2ab8801af15c65247c41b67bd6c8d75ff2ec8b05
Mashkov Volodymyr <103945286+ActaniumTime@users.noreply.github.com>
2025-01-26 23:58:52 +0200

    Build 0.0.15/1. Added styles for side-bar and small design.

--- app/views/EmpManagmentPanel/EmpManagTable.php
+++ app/views/EmpManagmentPanel/EmpManagTable.php
@@ -20,12 +20,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Employees</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'>
+
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../../public/css/sidebarStyle.css">
+    <link rel="stylesheet" href="../../../public/css/sidebarStyle2.css">
     <link rel="stylesheet" href="EmpManagTable.css">
 </head>
 <body>
 
-<?php
+<?php  
+    require_once __DIR__ . "../../partials/sideBar.php";
     require_once __DIR__ . "../../partials/tableEmp.php";
 ?>
 
--- app/views/partials/sideBar.php
+++ app/views/partials/sideBar.php
@@ -1,16 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Navbar Pure CSS</title>
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'>
-  <link rel="stylesheet" href="./style.css">
-</head>
-<body>
+<?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    require_once __DIR__ . '/../../../app/models/UserVerify.php';
+
+?>
 <div id="nav-bar">
   <input id="nav-toggle" type="checkbox"/>
   <div id="nav-header">
-    <a id="nav-title" href="https://codepen.io" target="_blank">C<i class="fab fa-codepen"></i>DEPEN</a>
+    <a id="nav-title" href="https://codepen.io" target="_blank">StudHRM</a>
     <label for="nav-toggle"><span id="nav-toggle-burger"></span></label>
     <hr/>
   </div>
@@ -30,188 +29,33 @@
   <div id="nav-footer">
     <div id="nav-footer-heading">
       <div id="nav-footer-avatar">
-        <img src="https://gravatar.com/avatar/4474ca42d303761c2901fa819c4f2547"/>
+        <img src="../../../Files/photos/<?php echo $emp->getAvatar(); ?>" alt="User Photo" class="rounded-circle" >
       </div>
       <div id="nav-footer-titlebox">
-        <a id="nav-footer-title" href="https://codepen.io/uahnbu/pens/public" target="_blank">uahnbu</a>
-        <span id="nav-footer-subtitle">Admin</span>
+        <a id="nav-footer-title" href="https://codepen.io/uahnbu/pens/public" target="_blank"><?php echo $emp->getName();?></a>
+        <span id="nav-footer-subtitle">
+        <?php 
+        switch($emp->getAccessLevelID()){
+          case 1:
+            echo "Admin";
+            break;
+          case 2:
+            echo "Manager";
+            break;
+          case 3:
+            echo "Employee";
+            break;
+          default:
+            echo "Unknown";
+            break;
+        }
+        ?>
+        </span>
       </div>
       <label for="nav-footer-toggle"><i class="fas fa-caret-up"></i></label>
     </div>
     <div id="nav-footer-content">
-      <Lorem>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Lorem>
+    <a href="../../../TEST_FILES/logout.php">Logout</a>
     </div>
   </div>
 </div>
-</body>
-</html>
-
-<style>
-:root {
-  --background: #f7f9fc;
-  --navbar-width: 256px;
-  --navbar-width-min: 80px;
-  --navbar-dark-primary: #eaeaf3;
-  --navbar-dark-secondary: #d6d8e1;
-  --navbar-light-primary: #5a5af7;
-  --navbar-light-secondary: #5a5af7;
-  --hover-highlight: #5a5af7;
-}
-
-html, body {
-  margin: 0;
-  background: var(--background);
-}
-
-#nav-bar {
-  position: absolute;
-  left: 1vw;
-  top: 1vw;
-  height: calc(100% - 2vw);
-  background: var(--navbar-dark-primary);
-  border-radius: 16px;
-  display: flex;
-  flex-direction: column;
-  color: var(--navbar-light-primary);
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  overflow: hidden;
-  user-select: none;
-}
-#nav-bar hr {
-  margin: 0;
-  position: relative;
-  left: 16px;
-  width: calc(100% - 32px);
-  border: none;
-  border-top: solid 1px var(--navbar-dark-secondary);
-}
-#nav-bar a {
-  color: inherit;
-  text-decoration: inherit;
-}
-
-#nav-header {
-  position: relative;
-  width: var(--navbar-width);
-  left: 16px;
-  width: calc(var(--navbar-width) - 16px);
-  min-height: 80px;
-  background: var(--navbar-dark-primary);
-  border-radius: 16px;
-  z-index: 2;
-  display: flex;
-  align-items: center;
-}
-#nav-header hr {
-  position: absolute;
-  bottom: 0;
-}
-
-#nav-title {
-  font-size: 1.5rem;
-}
-
-label[for=nav-toggle] {
-  position: absolute;
-  right: 0;
-  width: 3rem;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-}
-
-#nav-content {
-  margin: -16px 0;
-  padding: 16px 0;
-  position: relative;
-  flex: 1;
-  width: var(--navbar-width);
-  background: var(--navbar-dark-primary);
-  overflow-x: hidden;
-}
-
-.nav-button {
-  position: relative;
-  margin-left: 16px;
-  margin-right: 16px;
-  padding: 0px 16px;
-  height: 54px;
-  display: flex;
-  align-items: center;
-  color: var(--navbar-light-secondary);
-  cursor: pointer;
-  z-index: 1;
-  transition: background 0.2s, color 0.2s;
-  border-radius: 8px;
-}
-
-.nav-button:hover {
-  background: var(--hover-highlight);
-  color: var(--navbar-dark-primary);
-}
-
-.nav-button .fas {
-  min-width: 3rem;
-  text-align: center;
-}
-
-.nav-button span {
-  margin-left: 8px;
-}
-
-#nav-footer {
-  position: relative;
-  width: var(--navbar-width);
-  height: 54px;
-  background: var(--navbar-dark-secondary);
-  border-radius: 16px;
-  display: flex;
-  flex-direction: column;
-}
-
-#nav-footer-heading {
-  position: relative;
-  width: 100%;
-  height: 54px;
-  display: flex;
-  align-items: center;
-}
-
-#nav-footer-avatar {
-  position: relative;
-  margin: 11px 0 11px 16px;
-  left: 0;
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
-  overflow: hidden;
-}
-
-#nav-footer-avatar img {
-  height: 100%;
-}
-
-#nav-footer-titlebox {
-  position: relative;
-  margin-left: 16px;
-  width: 10px;
-  display: flex;
-  flex-direction: column;
-}
-
-#nav-footer-subtitle {
-  color: var(--navbar-light-secondary);
-  font-size: 0.6rem;
-}
-
-#nav-footer-content {
-  margin: 0 16px 16px 16px;
-  border-top: solid 1px var(--navbar-light-secondary);
-  padding: 16px 0;
-  color: var(--navbar-light-secondary);
-  font-size: 0.8rem;
-  overflow: auto;
-}
-</style>
