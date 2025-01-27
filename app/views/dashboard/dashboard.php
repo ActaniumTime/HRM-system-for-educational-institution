8ee6e7f02527bebca32728927af2b3615c29de86
Mashkov Volodymyr <103945286+ActaniumTime@users.noreply.github.com>
2025-01-27 15:43:02 +0200

    Build 0.0.15/3. Added styles for side-bar and small design.

--- app/views/dashboard/dashboard.php
+++ app/views/dashboard/dashboard.php
@@ -99,7 +99,19 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.3.1/dist/js/coreui.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        const navToggle = document.getElementById('nav-toggle');
+        const contentWrapper = document.querySelector('.content-wrapper');
 
+        navToggle.addEventListener('change', () => {
+            if (navToggle.checked) {
+                contentWrapper.style.marginLeft = `calc(var(--navbar-width-min) + 1rem)`;
+            } else {
+                contentWrapper.style.marginLeft = `calc(var(--navbar-width) + 1rem)`;
+            }
+        });
+
+    </script>
 </body>
 </html>
 
